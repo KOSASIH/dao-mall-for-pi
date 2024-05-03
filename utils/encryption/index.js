168c3ffd48d50852fac001525e5a3424db2cde12
KOSASIH <kosasihg88@gmail.com>
2024-05-04 01:45:31 +0700

    Create index.js

---
+++ utils/encryption/index.js
@@ -0,0 +1,15 @@
+// Implement encryption utilities
+
+// Example: Implement a function to encrypt data
+const encryptData = (data) => {
+  // Encrypt data
+  // Return encrypted data
+};
+
+// Example: Implement a function to decrypt data
+const decryptData = (encryptedData) => {
+  // Decrypt data
+  // Return decrypted data
+};
+
+export { encryptData, decryptData };
