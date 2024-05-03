4e9e6ba1eae14934d414360d1fb92872e0104248
KOSASIH <kosasihg88@gmail.com>
2024-05-04 01:48:01 +0700

    Create DAO.test.js

---
+++ tests/contracts/DAO.test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('DAO', () => {
+  let dao;
+  let owner;
+  let addr1;
+  let addr2;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    const DAO = await ethers.getContractFactory('DAO');
+    dao = await DAO.deploy();
+    await dao.deployed();
+  });
+
+  describe('createProposal', () => {
+    it('should create a proposal', async () => {
+      await dao.createProposal('Proposal 1');
+      const proposal = await dao.proposals(1);
+      expect(proposal.description).to.equal('Proposal 1');
+    });
+  });
+
+  describe('executeProposal', () => {
+    it('should execute a proposal', async () => {
+      await dao.createProposal('Proposal 1');
+      await dao.executeProposal(1);
+      const proposal = await dao.proposals(1);
+      expect(proposal.executed).to.equal(true);
+    });
+  });
+});
