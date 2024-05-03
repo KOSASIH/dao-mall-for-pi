17e740a39c8fbf28d26c5c80cb156a5206f5af58
KOSASIH <kosasihg88@gmail.com>
2024-05-04 01:10:17 +0700

    Create nftMarketplace.js

---
+++ api/v1/routes/nftMarketplace.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const nftMarketplaceController = require('../controllers/nftMarketplaceController');
+
+router.get('/', nftMarketplaceController.getMarketplaceInfo);
+router.post('/create', nftMarketplaceController.createNFT);
+router.post('/buy', nftMarketplaceController.buyNFT);
+
+module.exports = router;
