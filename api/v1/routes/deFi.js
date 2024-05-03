ab97598243df86c0fac35da43a12b9f653909429
KOSASIH <kosasihg88@gmail.com>
2024-05-04 01:10:55 +0700

    Create deFi.js

---
+++ api/v1/routes/deFi.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const deFiController = require('../controllers/deFiController');
+
+router.get('/lending', deFiController.getLendingInfo);
+router.get('/borrowing', deFiController.getBorrowingInfo);
+router.get('/yieldFarming', deFiController.getYieldFarmingInfo);
+
+module.exports = router;
